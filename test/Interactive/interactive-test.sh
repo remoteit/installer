bd68b199ada0b7e66b81747d5aa9dcc06aba237b
Gary Worsham <gary@remote.it>
2020-06-10 14:43:02 -0700

    Update interactive test to write debug info to /tmp/debug.txty

--- test/Interactive/interactive-test.sh
+++ test/Interactive/interactive-test.sh
@@ -13,11 +13,11 @@ checkForRoot
 # while capturing the debug output (e.g. for manual testing).
 if [ "$1" = "" ]; then
 #    sudo sh -x /usr/bin/connectd_installer 2> debug.txt | tee console.txt
-    sudo sh -x /usr/bin/connectd_installer | tee console.txt
+    sudo sh -x /usr/bin/connectd_installer 2> /tmp/debug.txt | tee /tmp/console.txt
 else
 # If run with one following parameter, use that parameter as the name of a keystroke file
 # such as configure-01.key, configure-02.key, remove-all.key
 #    sudo sh -x /usr/bin/connectd_installer < "$1" 2> debug.txt | tee console.txt
-    sudo sh -x /usr/bin/connectd_installer < "$1" | tee console.txt
+    sudo sh -x /usr/bin/connectd_installer < "$1" 2> /tmp/debug.txt | tee /tmp/console.txt
 fi
 
