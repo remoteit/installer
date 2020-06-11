893a301157d7a94282ce73ed4eb35806e05aafdf
Gary Worsham <gary@remote.it>
2020-06-11 10:27:03 -0700

    incorporating Benoit's comments from PR 112

--- test/Interactive/interactive-test.sh
+++ test/Interactive/interactive-test.sh
@@ -12,12 +12,10 @@ checkForRoot
 # If run with no following parameter, just run the script interactively
 # while capturing the debug output (e.g. for manual testing).
 if [ "$1" = "" ]; then
-#    sudo sh -x /usr/bin/connectd_installer 2> debug.txt | tee console.txt
     sudo sh -x /usr/bin/connectd_installer 2> /tmp/debug.txt | tee /tmp/console.txt
 else
 # If run with one following parameter, use that parameter as the name of a keystroke file
 # such as configure-01.key, configure-02.key, remove-all.key
-#    sudo sh -x /usr/bin/connectd_installer < "$1" 2> debug.txt | tee console.txt
     sudo sh -x /usr/bin/connectd_installer < "$1" 2> /tmp/debug.txt | tee /tmp/console.txt
 fi
 
